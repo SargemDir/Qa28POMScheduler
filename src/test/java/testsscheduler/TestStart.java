db7b8bea73e26acdc7d3fed5c99a2c1bfa14edba
TayaHatum <21taya21@gmail.com>
2021-08-19 21:50:11 +0300

    added Splash Screen

---
+++ src/test/java/testsscheduler/TestStart.java
@@ -0,0 +1,12 @@
+package testsscheduler;
+
+import config.ConfigScheduler;
+import org.testng.annotations.Test;
+
+public class TestStart extends ConfigScheduler {
+
+    @Test
+    public void teststart(){
+
+    }
+}
