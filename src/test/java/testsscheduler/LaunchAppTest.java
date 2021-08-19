d990837fd64df8b549d4bf2b6a4728ff3ef5e4ab
TayaHatum <21taya21@gmail.com>
2021-08-19 21:50:49 +0300

    added new Splash Screen

--- src/test/java/testsscheduler/LaunchAppTest.java
+++ src/test/java/testsscheduler/LaunchAppTest.java
@@ -9,6 +9,7 @@ public class LaunchAppTest extends ConfigScheduler {
 
     @Test
     public void launchtest(){
+
         String version = new SplashScreen(driver).getCurrentVersion();
         Assert.assertEquals(version,"0.0.3");
     }
